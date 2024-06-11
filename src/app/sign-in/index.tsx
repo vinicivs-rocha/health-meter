b2b0e5d19e98fc2fc0cc76d09a67cdd5368e9ac8
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-11 19:50:15 -0300

    refactor: fix sign-in route name

--- src/app/sign-in/index.tsx
+++ src/app/sign-in/index.tsx
@@ -1,6 +1,8 @@
 import { SignInViewModel } from "@/adapters/view-models/sign-in";
 import { useAppInjector } from "@/hooks/app-injector";
+import SignInController from "./controller";
 
 export default function SignInPage() {
   const viewModel = useAppInjector<SignInViewModel>("SignInViewModel");
+  return <SignInController viewModel={viewModel}></SignInController>;
 }
