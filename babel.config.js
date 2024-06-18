de28afd45ef407f9603e8b22372f3e6bed91cdf2
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-18 10:16:42 -0300

    feat: add env module declaration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    plugins: ["babel-plugin-transform-typescript-metadata", ['module:react-native-dotenv']],
+    plugins: [
+      "babel-plugin-transform-typescript-metadata",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
     presets: ["babel-preset-expo"],
   };
 };
