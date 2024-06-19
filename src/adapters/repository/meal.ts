9c15fe47a294b4e91337de1d68643d1b63988760
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-19 20:31:37 -0300

    feat: add meals finding by ids repository

---
+++ src/adapters/repository/meal.ts
@@ -0,0 +1,31 @@
+import {
+  MealData,
+  MealRepository,
+} from "@/domain/application/repositories/meal";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { id, inject, injectable } from "inversify";
+
+@injectable()
+export class SupabaseMealRepository implements MealRepository {
+  @inject("SupabaseClient") private readonly supabase!: SupabaseClient;
+  async findById(id: string): Promise<MealData | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findByIds(ids: string[]) {
+    const { data, error } = await this.supabase
+      .from("meals")
+      .select("id, name")
+      .in("id", ids);
+
+    if (error) {
+      throw error;
+    }
+
+    return data.map(({ id, name }) => ({
+      id,
+      name,
+      nutrionalValues: [],
+    }));
+  }
+}
