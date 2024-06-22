b3d7f1e8e3a640573672d09f0a05e68727f94301
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-22 15:19:56 -0300

    feat: add supervision view

--- src/app/supervision/[id].tsx
+++ src/app/supervision/[id].tsx
@@ -1,8 +1,11 @@
 import { StartSupervisionViewModel } from "@/adapters/view-models/start-supervision";
 import StartSupervisionController from "@/components/supervision/controller";
+import SupervisionHeader from "@/components/supervision/header";
+import SupervisionView from "@/components/supervision/view";
 import { useAppInjector } from "@/hooks/app-injector";
 import { SupervisionStore } from "@/stores/supervision";
 import { useLocalSearchParams } from "expo-router";
+import React from "react";
 
 export default function StartSupervisionPage() {
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -19,7 +22,14 @@ export default function StartSupervisionPage() {
     <StartSupervisionController
       userId={id}
       viewModel={viewModel}
-      store={store}
+      View={(props) => (
+        <SupervisionView
+          header={<SupervisionHeader store={store}></SupervisionHeader>}
+          highlightedMetric={<></>}
+          meals={<></>}
+          {...props}
+        ></SupervisionView>
+      )}
     ></StartSupervisionController>
   );
 }
