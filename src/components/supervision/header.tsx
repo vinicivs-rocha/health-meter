ff890699c823dce291145d8fa7807b42552f3c36
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-22 17:22:04 -0300

    feat: add header template

--- src/components/supervision/header.tsx
+++ src/components/supervision/header.tsx
@@ -1,10 +1,9 @@
 import { SupervisionStore } from "@/stores/supervision";
 import { MaterialIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar } from "expo-status-bar";
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
 import Bone from "../bone";
 
 interface SupervisionHeaderProps {
@@ -14,7 +13,6 @@ interface SupervisionHeaderProps {
 function Skeleton() {
   return (
     <>
-      <StatusBar style="dark" backgroundColor="#FDE047" translucent={true} />
       <LinearGradient
         start={{ x: 0, y: 1 }}
         end={{ x: 1, y: 0 }}
@@ -49,15 +47,51 @@ function SupervisionHeader({ store }: SupervisionHeaderProps) {
     return <Skeleton />;
   }
 
-  return <View></View>;
+  return (
+    <>
+      <LinearGradient
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 0 }}
+        colors={["#EAB308", "#FDE047"]}
+        style={styles.container}
+      >
+        <View style={styles.userDataContainer}>
+          <Image
+            source={{ uri: store.supervised?.photo }}
+            style={styles.userProfilePicture}
+          ></Image>
+          <View style={styles.titleContainer}>
+            <Text style={styles.greettingText}>Olá,</Text>
+            <Text style={styles.greettingName}>{store.supervised?.name}</Text>
+          </View>
+        </View>
+        <View style={styles.actionsContainer}>
+          <MaterialIcons
+            name="history"
+            size={20}
+            color={"68717A"}
+            disabled={true}
+          />
+          <MaterialIcons
+            name="exit-to-app"
+            size={20}
+            color={"68717A"}
+            disabled={true}
+          />
+        </View>
+      </LinearGradient>
+    </>
+  );
 }
 
 export default observer(SupervisionHeader);
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: -(StatusBar.currentHeight ?? 10),
     borderRadius: 8,
     padding: 20,
+    paddingTop: (StatusBar.currentHeight ?? 10) + 20,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -66,12 +100,25 @@ const styles = StyleSheet.create({
   userDataContainer: {
     flex: 1,
     flexDirection: "row",
-    gap: 8,
+    gap: 12,
   },
-  userProfilePicture: { width: 35, height: 35, borderRadius: 8 },
+  userProfilePicture: { width: 40, height: 40, borderRadius: 8 },
   titleContainer: {
     flex: 1,
     borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  greettingText: {
+    fontSize: 16,
+    fontFamily: "Poppins_400Regular",
+    color: "#343A40",
+  },
+  greettingName: {
+    fontSize: 20,
+    fontFamily: "Poppins_700Bold",
+    color: "#343A40",
+    marginTop: -8,
   },
   actionsContainer: {
     flexDirection: "row",
