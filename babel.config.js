7dc932298fef05baadc9719a1f847bc5a4874e29
vinicivs-rocha <viniciusroch65@gmail.com>
2024-06-24 21:38:53 -0300

    chore: add react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    plugins: ["babel-plugin-transform-typescript-metadata"],
+    plugins: [
+      "babel-plugin-transform-typescript-metadata",
+      "react-native-reanimated/plugin",
+    ],
     presets: ["babel-preset-expo"],
   };
 };
