343023c8c6312bec3af65947ac66e2442d84fc98
vinicivs-rocha <viniciusroch65@gmail.com>
2024-07-04 14:25:48 -0300

    feat: inject delete meal usecase to supervision viewmodel

--- src/adapters/view-models/start-supervision.ts
+++ src/adapters/view-models/start-supervision.ts
@@ -1,3 +1,7 @@
+import {
+  DeleteMeal,
+  DeleteMealInput,
+} from "@/domain/application/usecases/delete-meal";
 import { SignOut } from "@/domain/application/usecases/sign-out";
 import { StartSupervision } from "@/domain/application/usecases/start-supervision";
 import { inject, injectable } from "inversify";
@@ -6,6 +10,7 @@ import { inject, injectable } from "inversify";
 export class StartSupervisionViewModel {
   @inject("StartSupervision") private startSupervision!: StartSupervision;
   @inject("SignOut") private signOut!: SignOut;
+  @inject("DeleteMeal") loadMeals!: DeleteMeal;
 
   async start({ userId }: { userId: string }) {
     this.startSupervision.execute({ supervisedId: userId });
@@ -13,5 +18,9 @@ export class StartSupervisionViewModel {
 
   async logout() {
     this.signOut.execute();
+  }
+
+  async deleteMeal(data: DeleteMealInput) {
+    this.loadMeals.execute(data);
   }
 }
