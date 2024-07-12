7395a82a58b4993b6bbd82d15ea09c6ca9ad575a
Vinicius Rocha <viniciusroch65@gmail.com>
2024-07-12 13:20:14 -0300

    refactor: restructure code to new domain model

--- src/domain/application/usecases/sign-in.ts
+++ src/domain/application/usecases/sign-in.ts
@@ -1,13 +1,12 @@
+import { Supervised } from "@/domain/enterprise/entities/supervised";
 import { inject, injectable } from "inversify";
 import type { Usecase } from "../../../core/usecases/usecase";
-import type {
-  AuthenticationGateway,
-  UserData,
-} from "../gateways/authentication";
+import type { AuthenticationGateway } from "../gateways/authentication";
 import type { AuthenticationPresenter } from "../presenters/authentication";
+import { SupervisedRepository } from "../repositories/supervised";
 
 export type SignInOutput = {
-  userData: UserData;
+  supervised: Supervised;
 };
 
 @injectable()
@@ -17,17 +16,20 @@ export class SignIn implements Usecase<[], SignInOutput> {
   constructor(
     @inject("AuthenticationGateway")
     private readonly authenticationGateway: AuthenticationGateway,
+    @inject("SupervisedRepository")
+    private readonly supervisedRepository: SupervisedRepository,
     @inject("AuthenticationPresenter")
     private readonly authenticationPresenter: AuthenticationPresenter
   ) {}
 
   async execute() {
     try {
-      this.output = {
-        userData: await this.authenticationGateway.signIn(),
-      };
+      await this.authenticationGateway.signIn(),
+        (this.output = {
+          supervised: await this.supervisedRepository.findCurrent(),
+        });
 
-      this.authenticationPresenter.presentAuthenticated(this.output.userData);
+      this.authenticationPresenter.presentAuthenticated(this.output.supervised);
     } catch (error) {
       console.error(error);
     }
