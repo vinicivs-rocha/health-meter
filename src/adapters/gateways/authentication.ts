b1b17b039777908ff5fa0f60ac6645f6aafdf1c0
Vinicius Rocha <viniciusroch65@gmail.com>
2024-07-12 14:05:33 -0300

    refactor: restructure code to new domain model

--- src/adapters/gateways/authentication.ts
+++ src/adapters/gateways/authentication.ts
@@ -1,7 +1,4 @@
-import {
-  AuthenticationGateway,
-  UserData,
-} from "@/domain/application/gateways/authentication";
+import { AuthenticationGateway } from "@/domain/application/gateways/authentication";
 import { supabase } from "@/utils/supabase";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { SupabaseClient } from "@supabase/supabase-js";
@@ -10,43 +7,19 @@ import { inject, injectable } from "inversify";
 @injectable()
 export class SupabaseAuthenticationGateway implements AuthenticationGateway {
   @inject("SupabaseClient") private readonly supabase!: SupabaseClient;
-  async getStoredUser(): Promise<UserData | null> {
-    const {
-      data: { session },
-      error,
-    } = await this.supabase.auth.getSession();
 
-    if (error) {
-      throw error;
-    }
-
-    return (
-      session && {
-        photo: session.user.user_metadata.avatar_url,
-        name: session.user.user_metadata.name,
-        id: session.user.id,
-      }
-    );
-  }
-
-  async signIn(): Promise<UserData> {
+  async signIn(): Promise<void> {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
 
       if (userInfo.idToken) {
-        const { data, error } = await supabase.auth.signInWithIdToken({
+        const { error } = await supabase.auth.signInWithIdToken({
           provider: "google",
           token: userInfo.idToken,
         });
 
         if (error) throw error;
-
-        return {
-          id: data.user!.id,
-          photo: data.user.user_metadata.avatar_url,
-          name: data.user.user_metadata.name,
-        };
       }
       throw new Error("no ID token present!");
     } catch (error) {
@@ -57,5 +30,15 @@ export class SupabaseAuthenticationGateway implements AuthenticationGateway {
 
   async signOut(): Promise<void> {
     await this.supabase.auth.signOut();
+  }
+
+  async isAuthenticated(): Promise<boolean> {
+    const { data, error } = await this.supabase.auth.getSession();
+
+    if (error) {
+      throw error;
+    }
+
+    return data.session !== null;
   }
 }
