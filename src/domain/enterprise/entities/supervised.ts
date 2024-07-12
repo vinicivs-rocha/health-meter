cc9cd7f5473baffeb4dfb7c35c964626907ea850
Vinicius Rocha <viniciusroch65@gmail.com>
2024-07-12 09:12:33 -0300

    refactor: rebuild supervised entity

--- src/domain/enterprise/entities/supervised.ts
+++ src/domain/enterprise/entities/supervised.ts
@@ -1,72 +1,36 @@
 import { randomUUID } from "expo-crypto";
 import { Entity } from "../../../core/entities/entity";
 import type { Optional } from "../../../core/types/optional";
-import { Metric } from "../value-objects/metric";
-import { MetricIntake } from "../value-objects/metric-intake";
 import { Meal } from "./meal";
 
 export type SupervisedProps = {
   id: string;
   name: string;
-  highlightedMetric: Metric;
-  metrics: Metric[];
-  mealIds: string[];
+  metrics: string[];
+  meals: Meal[];
 };
 
 export class Supervised extends Entity<SupervisedProps> {
-  constructor(props: Optional<SupervisedProps, "id" | "mealIds">) {
+  constructor(props: Optional<SupervisedProps, "id">) {
     super({
       ...props,
       id: props.id ?? randomUUID(),
-      mealIds: [],
     });
-  }
-
-  set mealIds(mealIds: string[]) {
-    this.props.mealIds = mealIds;
   }
 
   get id() {
     return this.props.id;
   }
 
-  get metrics() {
-    return this.props.metrics;
-  }
-
   get name() {
     return this.props.name;
   }
 
-  get highlightedMetric() {
-    return this.props.highlightedMetric;
+  get metrics() {
+    return this.props.metrics;
   }
 
-  removeMeal(meal: Meal) {
-    this.props.mealIds = this.props.mealIds.filter(
-      (mealId) => mealId !== meal.id
-    );
-  }
-
-  subtractMetricIntakes(metricIntakes: MetricIntake[]) {
-    this.props.metrics = this.props.metrics.map((metric) => {
-      const metricIntake = metricIntakes.find(
-        (intake) => intake.metricId === metric.id
-      );
-      if (!metricIntake) return metric;
-
-      return new Metric(
-        metric.id,
-        metric.name,
-        metric.fieldName,
-        metric.unit,
-        metric.goal,
-        metric.intake - metricIntake.intake
-      );
-    });
-
-    this.props.highlightedMetric = this.props.metrics.find(
-      (metric) => metric.id === this.props.highlightedMetric.id
-    )!;
+  get meals() {
+    return this.props.meals;
   }
 }
